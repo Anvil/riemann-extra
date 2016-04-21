34140c2a51e0aded1b7c9572f8edabaf65dba737
Damien Nadé <github@livna.org>
2016-04-21 11:21:18 +0200

    project.clj: * bumping version to 0.2.4.anvil.2 * using riemann 0.2.11 * using org.clojure/clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.spootnik/riemann-extra "0.2.4.anvil.1"
+(defproject org.spootnik/riemann-extra "0.2.4.anvil.2"
   :description "utility functions for riemann"
   :url "https://github.com/pyr/riemann-extra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[riemann "0.2.10"]]}}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[riemann "0.2.11"]]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :omit-source false)
